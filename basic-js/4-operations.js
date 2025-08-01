94e42af51ffc013757ab401edd76c9affac94b13
gargiagarwal9057 <agarwalgargi1234@gmail.com>
2025-08-01 15:30:47 +0000

    operation configure

---
+++ basic-js/4-operations.js
@@ -0,0 +1,20 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+
+
+let str1="hello"
+let str2="hitesh"
+
+let str3= str1+str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+"2");
+console.log("1"+"2");
